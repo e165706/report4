583a22082ba67a985abb299546a8a8ea8f6344c8
Yamato Goto <e165706@ie.u-ryukyu.ac.jp>
2016-11-27 01:43:23 +0900

    report4

--- src/test/jp/ac/uryukyu/ie/e165706/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165706/EnemyTest.java
@@ -13,10 +13,9 @@ public class EnemyTest {
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.setDead(true);
-        for(int i=0; i<10; i++) {
+        for (int i = 0; i < 10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.getHitPoint());
     }
-
 }
